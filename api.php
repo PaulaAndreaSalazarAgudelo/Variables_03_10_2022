41a20cc5a4a34731bb2e29d0c58700d9f426df78
PaulaAndreaSalazarAgudelo <andreasalazaragudelo@gmail.com>
2022-10-10 15:17:40 -0500

    Archivos Base

---
+++ api.php
@@ -0,0 +1,20 @@
+<?php
+// Glosario de PHP
+    // Camel-case
+    // Extructura basica de los lenguajes de programacion
+    // Que es un seudo-Lenguaje programacion / un lengaje de programacion
+    // codigo limpio / codigo sucio
+    // Variables
+    // Maeras de escribir las variables en PHP
+    // 7 tipos de datos primitivos en PHP
+    // Declarar de Variable / Inicializacion de Variable
+    // Punto y coma
+    // palabras reservadas del sistema
+    // Que son los operadores
+    // Items en las coleciones de datos PHP
+    // echo / var_dump / print_r  
+    header('Access-Control-Allow-Origin: *');
+
+$nombreCompleto = "Paula Andrea Salazar Agudelo";
+$edad = '24';
+
